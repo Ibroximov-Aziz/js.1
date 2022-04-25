7df047d1c193acb1f841396aacb073f56121157b
Ibroximov-Aziz <ibrokhemovaziz@gmail.com>
2022-04-25 09:54:06 +0300

    add new file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+alert("Xush kelibsiz!");
+var questionName = prompt("Ismingiz nima?", );
+
+var lastName = prompt("Familiyangiz nima?", );
+
+var age = prompt("Tug'ilgan yilingiz", );
+
+var questionRule = prompt("Qayerda ishlaysiz");
+
+console.log("Ismingiz nima?");
+
+console.log("Familiyangiz nima?");
+
+console.log("Tug'ilgan yilingiz?");
